1e9cbb3ac43db448b0684eb715775e1a9a68a3b2
Hammad Afroz <hammadafroz007@gmail.com>
2024-06-08 13:31:46 +0530

    project started

---
+++ src/utils/contants.js
@@ -0,0 +1,12 @@
+const GOOGLE_API_KEY = "AIzaSyBGdDJVo7aDT6C8Cm8J0UU8mTGwF0EIE2E";
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBGdDJVo7aDT6C8Cm8J0UU8mTGwF0EIE2E&type=video&part=snippet&maxResults=10&q=";
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
